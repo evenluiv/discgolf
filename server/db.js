ee655da3572908f192c98288d325063f1894def1
Even Luiv <luiveven7@gmail.com>
2024-12-20 12:11:54 +0200

    connected db to backend

---
+++ server/db.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+require('dotenv').config()
+
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const database_name = process.env.DB_NAME;
+
+// Ensure password is a string
+if (typeof db_password !== 'string') {
+  console.error('Error: DB_PASSWORD is not a valid string');
+  process.exit(1);
+}
+
+// Create a new PostgreSQL connection pool
+const pool = new Pool({
+  user: db_user,
+  host: process.env.DB_HOST,
+  database: database_name,
+  password: db_password,
+  port: Number(process.env.DB_PORT), // Convert port to number
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-require("dotenv").config({ path: __dirname + "/.env" });
 const express = require('express');
+const pool = require('./db');
 
 const app = express();
 
@@ -8,8 +8,18 @@ const PORT = process.env.PORT || 9000;
 //Here you can add your routes
 //Here's an example
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+  res.send("Hello World!");
+});
+
+app.get('/api/courses', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM courses');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on the port ${PORT}`);
