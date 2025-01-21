c92c1bf846f19efa6c958b4a4feffabb5ae6e58f
Even Luiv <luiveven7@gmail.com>
2025-01-21 15:54:53 +0200

    edit config for backend functionality

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Example: React
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:9000', // Proxy API requests during local development
+    },
+  },
+  build: {
+    outDir: '../dist', // Output directory for production build
+  },
+});
