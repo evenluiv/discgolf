2d612b3593bc7774890df93b04e908058221a9e3
Even Luiv <luiveven7@gmail.com>
2025-01-21 16:00:33 +0200

    fixes to last commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../dist', // Output directory for production build
+    outDir: 'dist', // Output directory for production build
   },
 });
